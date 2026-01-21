f96c8444cc7b857a11ab365462bb99c002899fde
Praneeth13-2004 <karthikeyarsp@gmail.com>
2026-01-21 11:31:32 +0530

    Add emotion recognition using DeepFace and OpenCV
    Implement real-time emotion recognition using webcam input.

---
+++ DeepFaceEmotion/emotionRecognition.py
@@ -0,0 +1,43 @@
+import cv2
+from deepface import DeepFace
+
+cap = cv2.VideoCapture(0)
+
+if not cap.isOpened():
+    print("Error: Could not open webcam.")
+    exit()
+
+while True:
+    ret, frame = cap.read()
+    if not ret:
+        print("Error: Failed to capture frame from webcam.")
+        break
+
+    resized_frame = cv2.resize(frame, (1080,1080))
+
+    try:
+        result = DeepFace.analyze(img,actions=['emotion'])
+
+        if result:            
+            dominant_emotion = result[0]["dominant_emotion"]
+            cv2.putText(
+                frame,
+                f"Emotion: {dominant_emotion}",
+                (50, 50),
+                cv2.FONT_HERSHEY_SIMPLEX,
+                1,
+                (0, 255, 0),
+                2,
+                cv2.LINE_AA
+            )
+
+    except Exception as e:
+        print(f"Error analyzing frame: {e}")
+
+    cv2.imshow('Emotion Detection', frame)
+
+    if cv2.waitKey(1) & 0xFF == 27:  
+        break
+
+cap.release()
+cv2.destroyAllWindows()
